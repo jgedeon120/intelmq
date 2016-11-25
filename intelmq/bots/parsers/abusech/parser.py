71f618622b17eea8a670c2b7afe9ebe67e3c4480
jgedeon120 <joe.gedeon@gmail.com>
2016-11-25 10:12:09 -0500

    Single Abuse parser

--- intelmq/bots/parsers/abusech/parser.py
+++ intelmq/bots/parsers/abusech/parser.py
@@ -26,6 +26,8 @@ class AbusechParserBot(ParserBot):
                 'https://palevotracker.abuse.ch/blocklists.php?download=ipblocklist',
                 'https://zeustracker.abuse.ch/blocklist.php?download=badips'}
 
+    RANSOMWARE_FEEDS = {'https://ransomwaretracker.abuse.ch/feeds/csv/'}
+
     def parse_line(self, line, report):
         if line.startswith('#') or len(line) == 0:
             self.tempdata.append(line)
@@ -49,6 +51,38 @@ class AbusechParserBot(ParserBot):
                 event.add('source.ip', line)
                 event.add('raw', line)
                 event.add('malware.name', AbusechParserBot.SOURCE_FEEDS[report['feed.url']])
+
+            if report['feed.url'] in AbusechParserBot.RANSOMWARE_FEEDS:
+                value = line.split(',')
+                if '|' in value[7]:
+                    for ipaddr in value[7].strip('"').split('|'):
+                        new_line = value[0].strip('"') + ',' + value[1].strip('"') + ',' + value[3].strip('"') \
+                                   + ',' + value[4].strip('"') + ',' + value[5].strip('"') + ',' + value[6].strip('"') \
+                                   + ',' + ipaddr + ',' + value[8].strip('"') + ',' + value[9].strip('"')
+
+                        value = new_line.split(',')
+                        event.add('classification.identifier', value[2].lower())
+                        event.add('classification.type', 'c&c')
+                        event.add('time.source', value[0] + ' UTC', force=True)
+                        event.add('status', value[5])
+                        event.add('source.ip', value[7])
+                        event.add('raw', line)
+                        if FQDN.is_valid(value[3]):
+                            event.add('source.fqdn', value[3])
+                        if URL.is_valid(value[4]):
+                            event.add('source.url', value[4])
+                else:
+                    event.add('classification.identifier', value[2].lower())
+                    event.add('classification.type', 'c&c')
+                    event.add('time.source', value[0] + ' UTC')
+                    event.add('status', value[5])
+                    event.add('raw', line)
+                    if IPAddress.is_valid(value[7]):
+                        event.add('source.ip', value[7])
+                    if FQDN.is_valid(value[3]):
+                        event.add('source.fqdn', value[3])
+                    if URL.is_valid(value[4]):
+                        event.add('source.url', value[4])
 
         yield event
 
